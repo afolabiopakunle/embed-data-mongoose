f66f8c0ee77645811d803fbc5532c929b7710350
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-16 18:25:07 +0100

    postSchema and userSchema created

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ mongoose.connect("mongodb://localhost/data_test", {
     useFindAndModify: false
 });
 
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+const Post = mongoose.model("Post", postSchema);
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     posts: [postSchema]
-})
+});\
+const User = mongoose.model("User", userSchema)
